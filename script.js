b8b1b31a134b475a544227038b17d5ee8295c43a
John Linnane <johnlinnane@Johns-iMac.local>
2020-05-05 17:03:47 +0100

    shuffle function added

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ function init() {
 }
 
 
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -21,45 +37,58 @@ function showInfo(data, tabletop) {
 
     console.log(getRandomInt(data.length));
 
+    // console.log(data);
 
-    let randIntQ = getRandomInt(data.length);
+
+    let shufArr = shuffle(data);
+    // console.log(shufArr);
+
+    let newArr = [shufArr[0], shufArr[1], shufArr[2]];
+    // console.log(newArr);
+
+    let rand = getRandomInt(newArr.length);
+
+
+    // ********************** PRINT QUESTION **********************
+
 
     let q = document.createElement("div");
     q.innerHTML = "Q.";
 
 
     let qDiv = document.createElement("div");
-    qDiv.innerHTML = data[randIntQ].question;
-    qDiv.setAttribute("id", `question${randIntQ}`);
+    qDiv.innerHTML = newArr[rand].question;
+    qDiv.setAttribute("id", `question`);
     display.appendChild(qDiv);
 
+    // ********************** SHOW ANSWERS **********************
 
     let a = document.createElement("div");
     a.innerHTML = "ANSWERS:";
 
 
-    let randIntA1 = getRandomInt(data.length);
+    // let randIntA1 = getRandomInt(data.length);
 
 
-    let a1 = document.createElement("div");
-    a1.innerHTML = data[randIntA1].answer;
-    a1.setAttribute("id", `answer${randIntA1}`);
+    let a1 = document.createElement("button");
+    a1.innerHTML = newArr[0].answer;
+    a1.setAttribute("id", `answer0`);
     display.appendChild(a1);
 
-    let randIntA2 = getRandomInt(data.length);
+    // let randIntA2 = getRandomInt(data.length);
 
 
-    let a2 = document.createElement("div");
-    a2.innerHTML = data[randIntA2].answer;
-    a2.setAttribute("id", `answer${randIntA2}`);
+    let a2 = document.createElement("button");
+    a2.innerHTML = newArr[1].answer;
+    a2.setAttribute("id", `answer1`);
     display.appendChild(a2);
 
-    let randIntA3 = getRandomInt(data.length);
+    // let randIntA3 = getRandomInt(data.length);
 
 
-    let a3 = document.createElement("div");
-    a3.innerHTML = data[randIntA3].answer;
-    a3.setAttribute("id", `answer${randIntA3}`);
+    let a3 = document.createElement("button");
+    a3.innerHTML = newArr[2].answer;
+    a3.setAttribute("id", `answer2`);
     display.appendChild(a3);
 
 
