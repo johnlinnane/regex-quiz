a549956deb025bb7c6cd7d9446296dbeaf90e498
John Linnane <johnlinnane@Johns-iMac.local>
2020-05-05 22:28:18 +0100

    fixed onclick

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ function shuffle(array) {
 
 
 function showInfo(data, tabletop) {
-
+    console.log(data);
     const shufArr = shuffle(data);
 
 
-
     shufArr.forEach(function(index, i) {
-        
+        // console.log(shufArr);
+
    
         let wrongAns = shufArr.filter(item => item !== shufArr[i]);
         wrongAns = shuffle(wrongAns);
-        console.log(wrongAns);
+        // console.log(wrongAns);
         
         let choices = [];
         choices[0] = shufArr[i].answer;
@@ -51,18 +51,33 @@ function showInfo(data, tabletop) {
         q.innerHTML = index.question;
         container.appendChild(q);
 
-        for (var i = 0; i < 3; i++) {
+
+        for (var j = 0; j < 3; j++) {
             let a = document.createElement("div");
             container.appendChild(a);
 
             let butt = document.createElement("button");
-            butt.innerHTML = choicesShuf[i];
+            butt.innerHTML = choicesShuf[j];
+
+            let choice;
+
+            if (j == 0) {
+                choice = choicesShuf[j];
+            } else if (j == 1) {
+                choice = choicesShuf[j];
+            } else {
+                choice = choicesShuf[j];
+            }
+            
+ 
+
             
 
             a.appendChild(butt);
             butt.onclick = function(){
-                console.log(choicesShuf[i]);
-                if (choicesShuf[i] == shufArr[i].answer) {
+                console.log(choice);
+
+                if (choice == shufArr[i].answer) {
                     alert('Correct!')
                 } else {
                     alert('Wrong!')
