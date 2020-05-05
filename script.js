f3f574c3642a3f8810e55386e3b2004ed9477cb8
John Linnane <johnlinnane@Johns-iMac.local>
2020-05-05 18:01:17 +0100

    foreach

--- script.js
+++ script.js
@@ -29,96 +29,41 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-var display = document.getElementById("display");                    
+
+
 
 
 
 function showInfo(data, tabletop) {
 
-    console.log(getRandomInt(data.length));
+    const shufArr = shuffle(data);
 
-    // console.log(data);
+    shufArr.forEach(function(index, i) {
+        
 
+        let ansArr = [];
 
-    let shufArr = shuffle(data);
-    // console.log(shufArr);
+        ansArr[0] = index.answer;
+        ansArr[1] = 'Answer Two';
+        ansArr[2] = 'Answer Three';
 
-    let newArr = [shufArr[0], shufArr[1], shufArr[2]];
-    // console.log(newArr);
+        let q = document.createElement("div");
+        q.innerHTML = index.question;
+        container.appendChild(q);
 
-    let rand = getRandomInt(newArr.length);
+        for (var i = 0; i < 3; i++) {
+            let a = document.createElement("div");
+            // a.innerHTML = ansArr[i];
+            container.appendChild(a);
 
-
-    // ********************** PRINT QUESTION **********************
-
-
-    let q = document.createElement("div");
-    q.innerHTML = "Q.";
-
-
-    let qDiv = document.createElement("div");
-    qDiv.innerHTML = newArr[rand].question;
-    qDiv.setAttribute("id", `question`);
-    display.appendChild(qDiv);
-
-    // ********************** SHOW ANSWERS **********************
-
-    let a = document.createElement("div");
-    a.innerHTML = "ANSWERS:";
-
-
-    // let randIntA1 = getRandomInt(data.length);
-
-
-    let a1 = document.createElement("button");
-    a1.innerHTML = newArr[0].answer;
-    a1.setAttribute("id", `answer0`);
-    display.appendChild(a1);
-
-    // let randIntA2 = getRandomInt(data.length);
-
-
-    let a2 = document.createElement("button");
-    a2.innerHTML = newArr[1].answer;
-    a2.setAttribute("id", `answer1`);
-    display.appendChild(a2);
-
-    // let randIntA3 = getRandomInt(data.length);
-
-
-    let a3 = document.createElement("button");
-    a3.innerHTML = newArr[2].answer;
-    a3.setAttribute("id", `answer2`);
-    display.appendChild(a3);
+            let butt = document.createElement("button");
+            butt.innerHTML = ansArr[i];
+            a.appendChild(butt);
+        }
 
 
 
-
-
-    
-
-    // data.forEach(function(data, i) {
-
-    //     let qDiv = document.createElement("div");
-    //     qDiv.innerHTML = data.question;
-    //     qDiv.setAttribute("id", `question${i}`);
-    //     display.appendChild(qDiv);
-
-    //     let aDiv = document.createElement("div");
-    //     aDiv.innerHTML = data.answer;
-    //     aDiv.setAttribute("id", `answer${i}`);
-    //     display.appendChild(aDiv);
-
-
-
-    //     let br = document.createElement("br")
-    //     display.appendChild(br);
-
-    // });
+    })
 }
 
-
-
 window.addEventListener('DOMContentLoaded', init);
-
-
